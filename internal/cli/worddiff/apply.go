dec600981e29b222690c057a04edece543ea5a9b
hachi-n <hachi-n@hachi-n-mac.local>
2021-11-15 22:23:07 +0900

    Fix typo.

--- internal/cli/worddiff/apply.go
+++ internal/cli/worddiff/apply.go
@@ -7,8 +7,8 @@ import (
 
 func Apply(fileName1, fileName2 string) error {
 	comparator := model.NewComparator(
-		model.NewComparisonFile(fileName1, loader.LoadWithFailOnExit(fileName1)),
-		model.NewComparisonFile(fileName2, loader.LoadWithFailOnExit(fileName2)),
+		model.NewComparisonFile(fileName1, loader.LoadWithExitOnFail(fileName1)),
+		model.NewComparisonFile(fileName2, loader.LoadWithExitOnFail(fileName2)),
 	)
 	return comparator.PrintWordDiff()
 }
--- internal/loader/loader.go
+++ internal/loader/loader.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func LoadWithFailOnExit(fileName string) []byte {
+func LoadWithExitOnFail(fileName string) []byte {
 	b, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		fmt.Println(err)
